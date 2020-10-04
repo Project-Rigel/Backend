710cb576c8d321b39d2118812b59a2c402e2780f
Edu Simon <edusimonpicon@gmail.com>
2020-10-04 10:58:00 +0200

    fix CD for firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,5 @@
 import * as admin from 'firebase-admin';
 import 'reflect-metadata';
-import { setAgendaScheduleSettings } from './agendas/infrastructure/functions/add-schedule-settings-to-agenda';
-import { GetAgendaConfigFunction } from './agendas/infrastructure/functions/get-agenda-config.function';
-import { getAvailableIntervalsInDayFunction } from './agendas/infrastructure/functions/get-available-intervals-in-day.function';
-import { getOpenDaysForMonth } from './agendas/infrastructure/functions/get-open-days-for-month';
-import { setAgendaConfigFunction } from './agendas/infrastructure/functions/set-agenda-config.function';
-import { bookAppointmentFunction } from './appointments/infrastructure/book-appointment.function';
-import { createCustomClaimOnBusinessCreationFunction } from './users/add-custom-claim.function';
 
 //important to run the init first
 if (process.env.FUNCTIONS_EMULATOR) {
@@ -17,6 +10,14 @@ if (process.env.FUNCTIONS_EMULATOR) {
 } else {
   admin.initializeApp();
 }
+
+import { setAgendaScheduleSettings } from './agendas/infrastructure/functions/add-schedule-settings-to-agenda';
+import { GetAgendaConfigFunction } from './agendas/infrastructure/functions/get-agenda-config.function';
+import { getAvailableIntervalsInDayFunction } from './agendas/infrastructure/functions/get-available-intervals-in-day.function';
+import { getOpenDaysForMonth } from './agendas/infrastructure/functions/get-open-days-for-month';
+import { setAgendaConfigFunction } from './agendas/infrastructure/functions/set-agenda-config.function';
+import { bookAppointmentFunction } from './appointments/infrastructure/book-appointment.function';
+import { createCustomClaimOnBusinessCreationFunction } from './users/add-custom-claim.function';
 
 exports.getAvaliableTimeIntervals = getAvailableIntervalsInDayFunction;
 exports.bookAppointment = bookAppointmentFunction;
